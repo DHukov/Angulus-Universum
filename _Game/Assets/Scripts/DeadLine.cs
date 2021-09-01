16142476b773d3513bdd51564a217490f8ad4228
DHukov <48653308+DHukov@users.noreply.github.com>
2021-09-01 12:16:50 +0200

    First our project v1.0
    -Added project to Repository
    
    Project has been made in 2019

---
+++ _Game/Assets/Scripts/DeadLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+       // Destroy(collision.gameObject);
+        other.gameObject.transform.localPosition = new Vector3(30, 1, 1);
+
+    }
+}
---
+++ _Game/Assets/Scripts/M_Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(1f, 100f)] [SerializeField] float movementSpeed = 1f;
+    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float jumpForce = 3f;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] Transform raycastOrigin;
+
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movementSpeed = 4f;
+        }
+        */
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 105.0f * rotationSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * movementSpeed;
+        transform.Rotate(xAngle: 0, yAngle: x, zAngle: 0);
+        transform.Translate(x: 0, y: 0, z);
+        if (Input.GetKeyDown(KeyCode.Space) && canIjump())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool canIjump()
+    {
+        return Physics.Raycast(raycastOrigin.position, direction: Vector3.down, maxDistance: 0.1f);
+    }
+
+}
